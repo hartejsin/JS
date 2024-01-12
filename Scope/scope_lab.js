84bc03f20c6762b2829fd62360a645962520cf37
Hartej Singh <h2singh@ucsd.edu>
2024-01-12 13:36:41 -0500

    control flow

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -7,15 +7,20 @@ const globalConst = "I'm a global constant";
 {
 // Block scope
 var blockVar = "I'm a block-scoped var";
+
 let blockLet = "I'm a block-scoped let";
+
 const blockConst = "I'm a block-scoped const";
 }
-function show(){
-    var functionVar = "I'm a block-scoped var";
-    let functionLet = "I'm a block-scoped let";
-    const functionConst = "I'm a block-scoped const";
-    }
-show();
+
+console.log(blockVar)
+
+//function show(){
+    //var functionVar = "I'm a block-scoped var";
+    //let functionLet = "I'm a block-scoped let";
+    //const functionConst = "I'm a block-scoped const";
+   // }
+///show();
     
 //console.log(functionVar); // Throws ReferenceError
 //console.log(functionLet); // Throws ReferenceError
